6ab98d720cf5c60b3da308e41cfaa24853e579a7
Danny Mora <danny.f.Mora.-ND@disney.com>
2022-10-24 23:57:04 -0500

    Crear un componente test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { TestComponent } from './shared/test/test.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ErrorPageComponent
+    ErrorPageComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
